a1c1deb04a0dabca0f0b510d3fc2f98b9e3f9b79
Tjstevens19 <tdogstrike@gmail.com>
2024-02-08 12:18:29 -0700

    Add project files.

---
+++ Mission5_Stevens/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission5_Stevens.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
