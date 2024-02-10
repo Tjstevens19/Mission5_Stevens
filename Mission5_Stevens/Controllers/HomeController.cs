1f0a29869c43244d88a58acdb3295a5dea4e3242
Tjstevens19 <tdogstrike@gmail.com>
2024-02-10 13:21:41 -0700

    Remove unecessary stuff

--- Mission5_Stevens/Controllers/HomeController.cs
+++ Mission5_Stevens/Controllers/HomeController.cs
@@ -24,9 +24,5 @@ namespace Mission5_Stevens.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
